7b8a4ccf9ef9c0f75a8bba53ab286335c855c382
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-07-01 11:38:48 +0530

    rest-api

---
+++ deno-rest-api/controllers/products.ts
@@ -0,0 +1,22 @@
+interface Product {
+    id:string;
+    name:string;
+    description:string;
+    price:number;
+}
+
+
+let products = Product[]=[
+    {
+        id:'1',
+        name:'First',
+        description:'Good Product',
+        price:29.99,
+    },
+    {
+        id:'2',
+        name:'second',
+        description:'Bad Product',
+        price:10
+    }
+]
