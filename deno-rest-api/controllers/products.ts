d9590794ec0bb0917b5d199751ae5cf071002154
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-07-01 12:13:25 +0530

    rest-api-innit

--- deno-rest-api/controllers/products.ts
+++ deno-rest-api/controllers/products.ts
@@ -1,22 +1,65 @@
-interface Product {
-    id:string;
-    name:string;
-    description:string;
-    price:number;
-}
+import { Product } from "../types.ts";
 
+let products: Product[] = [
+  {
+    id: "1",
+    name: "First",
+    description: "Good Product",
+    price: 29.99,
+  },
+  {
+    id: "2",
+    name: "second",
+    description: "Bad Product",
+    price: 10,
+  },
+];
 
-let products = Product[]=[
-    {
-        id:'1',
-        name:'First',
-        description:'Good Product',
-        price:29.99,
-    },
-    {
-        id:'2',
-        name:'second',
-        description:'Bad Product',
-        price:10
+///@desc    GET all products
+///@route   GET /api/v1/products
+
+const getProducts = ({ response }: { response: any }) => {
+  response.body = {
+    success: true,
+    data: products,
+  };
+};
+
+///@desc    GET one products
+///@route   GET /api/v1/products/id
+const getProduct = (
+  { params, response }: { params: { id: string }; response: any },
+) => {
+    const product:Product|undefined =  products.find(p=> p.id ===params.id)
+
+    if(product){
+        response.status = 200
+        response.body ={
+            success :true,
+            data:product,
+        }
     }
-]
+    else{
+        response.status = 404
+        response.body ={
+            success :false,
+        }
+    }
+};
+
+///@desc    Add one products
+///@route   Post /api/v1/products
+const addProduct = ({ response }: { response: any }) => {
+};
+
+///@desc    uppdate one products
+///@route   PUT /api/v1/products/:id
+const updateProduct = ({ response }: { response: any }) => {
+};
+
+///@desc    delete one products
+///@route   DEL /api/v1/products/:id
+const deleteProduct = ({ response }: { response: any }) => {
+};
+
+export { getProducts, getProduct, addProduct, updateProduct, deleteProduct };
